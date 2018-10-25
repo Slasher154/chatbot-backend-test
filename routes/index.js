10e7c4c47190e95d762a0ca22ab5c81f46d42ace
Thanat Varathon <thanatv@thaicom.net>
2018-10-25 17:25:52 +0700

    Add console log

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+  console.log(JSON.stringify(req.body, undefined, 2)
   res.json({ title: 'Welcomes' })
 });
 
